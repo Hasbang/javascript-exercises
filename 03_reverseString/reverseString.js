06eb869614ec684150cbb585bd31ab40413d1665
Hassan <hassanabangura1@gmail.com>
2024-08-17 10:37:39 +0000

    first three excercises  completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    // split the string into array of characters 
+   let splitString = str.split("");
+
+   // revserse the array of characters 
+   let reverseArray = splitString.reverse();
+
+   // join the reverse array of characters 
+   let reversedString = reverseArray.join("");
+
+   return reversedString;
 
 };
+let originalString = "Hello there";
+let reversed = reverseString(originalString);
+console.log(reversed);
+
 
 // Do not edit below this line
 module.exports = reverseString;
